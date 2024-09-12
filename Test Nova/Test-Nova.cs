62bb52eef56849d6220a9320b1a3c350cd5e01fe
Denis <103124554+DenStayy@users.noreply.github.com>
2024-09-12 17:56:41 +0200

    Add files via upload

---
+++ Test Nova/Test-Nova.cs
@@ -0,0 +1,18 @@
+﻿using Life;
+using UnityEngine;
+
+namespace Test_Nova
+{
+    public class TestNova : Plugin
+    {
+        public TestNova(IGameAPI aPI) : base(aPI)
+        {
+        }
+
+        public override void OnPluginInit()
+        {
+            base.OnPluginInit();
+            Debug.Log("Test Nova est initialisée avec succées !");
+        }
+    }
+}
